cffb095bc5c613b6248a3dd37693e0cc91f842bd
Swetha Kanumuri <swetha.kanumuri25@gmail.com>
2026-07-08 16:28:37 -0500

    Print DB debug

--- src/main/java/com/swetha/ecommerce/util/DBConnection.java
+++ src/main/java/com/swetha/ecommerce/util/DBConnection.java
@@ -24,6 +24,11 @@ public class DBConnection {
 
             Class.forName("com.mysql.cj.jdbc.Driver");
 
+            System.out.println("URL = " + URL);
+            System.out.println("USER = " + USER);
+            System.out.println("PASSWORD NULL? " + (PASSWORD == null));
+            System.out.println("PASSWORD LENGTH = " + (PASSWORD == null ? 0 : PASSWORD.length()));
+
             con = DriverManager.getConnection(URL, USER, PASSWORD);
 
             System.out.println("Database Connected Successfully!");
@@ -31,8 +36,13 @@ public class DBConnection {
         } catch (Exception e) {
 
             System.out.println("DATABASE CONNECTION FAILED");
+        
+            System.out.println(e.getClass().getName());
+        
+            System.out.println(e.getMessage());
+        
             e.printStackTrace();
-
+        
         }
 
         return con;
